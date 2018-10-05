97891eee7b3db58cc0da46c92a69f65a5aadd952
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-10-05 23:42:48 +0300

    JavaScript learning (add elements)

--- js/main_js.js
+++ js/main_js.js
@@ -1,4 +1,10 @@
-function greetMe(myName){
-    alert("Hello" + myName);
+function addScript(src){
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.appendChild(script);
 }
-greetMe("World, this is JS code")
+addScript('../js/1.js');
+addScript('../js/2.js');
+addScript('../js/3.js');
+addScript('../js/4.js');
