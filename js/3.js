64ef26ebcb36f96d26711a4a9bb10a6b5eeb20c4
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-10-16 00:03:17 +0300

    Removed old code

--- js/3.js
+++
@@ -1,1 +0,0 @@
-alert("Message #3");
--- js/main_js.js
+++ js/main_js.js
@@ -1,10 +1,5 @@
-function addScript(src){
-    var script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.appendChild(script);
-}
-addScript('../js/1.js');
-addScript('../js/2.js');
-addScript('../js/3.js');
-addScript('../js/4.js');
+"use strict";
+var admin, name;
+name = "Mike";
+admin = "Luke";
+alert(name, admin);
