61caae2db86887c1b62cd18f7e1d690aa46ac7cc
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-10-24 22:22:32 +0300

    Learning JS

--- js/main_js.js
+++ js/main_js.js
@@ -4,3 +4,29 @@ function calcSq(){
     squareNumber = inputNumber*inputNumber;
     alert(squareNumber);
 }
+
+// //Modal windows
+// //Prompt
+// var howOldAreYou;
+// howOldAreYou = prompt("How old are you?", "");
+// alert("You are " + howOldAreYou);
+
+// //confirm
+// var admin;
+// admin = confirm("Are you Admin?");
+// alert(admin);
+
+//What is your name?
+var user, pass;
+user = prompt("Who are you?", "");
+if (user == "Oleksii"){
+    alert("Welcome Admin");
+    pass = prompt("Enter password", "");
+    if (pass == "Dark"){
+        alert("Welcome to home page");
+    } else if (pass == null){//(*)
+        alert("Canceled enetering");
+    } else {
+        alert("Wrong Password");
+    }
+}
