86d2d789cd1cbaa77c787892af74ce595665aa19
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-10-26 14:40:10 +0300

    Learn JS

--- js/main_js.js
+++ js/main_js.js
@@ -1,18 +1,8 @@
-function calcSq(){
-    var inputNumber, squareNumber;
-    inputNumber = document.getElementById("input").value;
-    squareNumber = inputNumber*inputNumber;
-    alert(squareNumber);
+function clickBut(){
+    var p;
+    p = document.getElementById("ModP");
+    //p.innerHTML = "Hello!";
+    //p.innerHTML += "Hello World!";
+    //p.innerHTML = p.innerHTML +" <b>Hello Users!</b>";
+    p.insertAdjacentHTML("afterend","New Word");
 }
-
-var age, young, old;
-young = 14;
-old = 90;
-age = prompt ("How old are you?");
-    if (age<=young){
-        alert("Your age less then 14.");
-    } else if(age>=old){
-            alert("Your age more then 90.");
-        }
-
-
