cc43c43f38477d2abc77341f33dd6c1d9855fb8e
Xackspel <42944802+Xackspel@users.noreply.github.com>
2018-10-31 22:48:17 +0200

    Learn JS

--- js/main_js.js
+++ js/main_js.js
@@ -1,9 +1,10 @@
 function clickBut(){
-    var p;
+    var p, i;
+    i = 0;
     p = document.getElementById("ModP");
-    //p.innerHTML = "Hello!";
-    //p.innerHTML += "Hello World!";
-    //p.innerHTML = p.innerHTML +" <b>Hello Users!</b>";
-    p.insertAdjacentHTML("afterend"," New Word");
-    console.log(typeof(p));
+    while(i<10){
+        p.insertAdjacentHTML("afterend"," New Word");
+        i++
+    }
 }
+
